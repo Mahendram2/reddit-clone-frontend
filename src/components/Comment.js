cb4c3df6942fe85370e06047a6d30b749a61e1a6
Seth Miller <millerado@gmail.com>
2022-09-22 13:55:46 -0600

    create basic component

---
+++ src/components/Comment.js
@@ -0,0 +1,5 @@
+function Comment(props) {
+  return <h2>I am a comment!</h2>;
+}
+
+export default Comment;
---
+++ src/components/Content.js
@@ -0,0 +1,5 @@
+function Content(props) {
+  return <h2>I am the content of a post!</h2>;
+}
+
+export default Content;
