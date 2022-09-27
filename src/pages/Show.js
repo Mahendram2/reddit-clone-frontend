49e37ea474ece71f466b4f9c430a62bde81a1ec9
Seth Miller <millerado@gmail.com>
2022-09-27 11:01:16 -0600

    set up form for comments. create handleSubmit/handleChange. Update initial state

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,9 @@
 import { useParams, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-function Show({ feed, deletePost }) {
+function Show({ feed, deletePost, createComment }) {
+  const [newComment, setNewComment] = useState({ body: '', userID: '' });
+
   const { id } = useParams();
   const post = feed ? feed.find((p) => p._id === id) : null;
 
@@ -11,11 +14,36 @@ function Show({ feed, deletePost }) {
     navigate('/');
   };
 
+  const handleSubmit = () => {
+    createComment(newComment, id);
+  };
+
+  const handleChange = (e) => {
+    setNewComment({
+      ...newComment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(post);
+
   return (
     <div className='show-post'>
       <p>Made by: {post.createdBy}</p>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='body'
+          value={newComment.body}
+          onChange={handleChange}
+        />
+        <input type='submit' value='Comment' />
+      </form>
+      {post.replies.map((content) => (
+        <p>content</p>
+      ))}
       <button onClick={handleDelete}>Delete Post</button>
     </div>
   );
