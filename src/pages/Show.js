9df47d9ee5e639cdf8fad283d0c1f0057d94bb6c
Seth Miller <millerado@gmail.com>
2022-09-27 11:10:48 -0600

    create loading and loaded functions to render if posts exists or not

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -27,26 +27,34 @@ function Show({ feed, deletePost, createComment }) {
 
   console.log(post);
 
-  return (
-    <div className='show-post'>
-      <p>Made by: {post.createdBy}</p>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          name='body'
-          value={newComment.body}
-          onChange={handleChange}
-        />
-        <input type='submit' value='Comment' />
-      </form>
-      {post.replies.map((content) => (
-        <p>content</p>
-      ))}
-      <button onClick={handleDelete}>Delete Post</button>
-    </div>
-  );
+  const loading = () => {
+    return <h1>Loading Post...</h1>;
+  };
+
+  const loaded = () => {
+    return (
+      <div className='show-post'>
+        <p>Made by: {post.createdBy}</p>
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='body'
+            value={newComment.body}
+            onChange={handleChange}
+          />
+          <input type='submit' value='Comment' />
+        </form>
+        {post.replies.map((content) => (
+          <p>content</p>
+        ))}
+        <button onClick={handleDelete}>Delete Post</button>
+      </div>
+    );
+  };
+
+  return post ? loaded() : loading();
 }
 
 export default Show;
