f95f1e6cc1c796ad2a34ac44d2ef4d6ca31e5e57
Seth Miller <millerado@gmail.com>
2022-09-28 12:16:54 -0600

    display posts with category 'Movies'

--- src/pages/Boards.js
+++ src/pages/Boards.js
@@ -4,18 +4,20 @@ function Boards({ feed, createdTime, user }) {
   };
 
   const loaded = () => {
-    const filterPosts = (cat) => {
+    function filterPosts(cat) {
       const filteredPosts = feed.filter((p) => p.category === cat);
-      return filterPosts;
-    };
+      return filteredPosts;
+    }
+
+    const moviePosts = filterPosts('Movies');
 
     return (
       <div className='boards'>
         <h1>Boards</h1>
         <div className='board'>
           <h2>Movies</h2>
-          {filterPosts(Movies).map((post) => (
-            <h3>{post.title}</h3>
+          {moviePosts.map((post) => (
+            <h3 key={Math.random}>{post.title}</h3>
           ))}
         </div>
       </div>
