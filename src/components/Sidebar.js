cc729e4d343c90fefa0d67c5a321760f48130018
ian-brown-86 <an.ian.brown@gmail.com>
2022-09-29 15:31:57 -0400

    wip: alters sidebar and adds style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,34 +3,18 @@ import { Link } from 'react-router-dom';
 export default function Sidebar(props) {
   return (
     <div className='sb-container'>
-      <div className='create'>
-        <a href='./newpost'>
-          <button type='button' className='create-b'>
-            Create Post
-          </button>
-        </a>
-      </div>
-      <div className='boards-link'>
-        <a href='./boards'>
-          <button type='button' className='create-b'>
-            Boards
-          </button>
-        </a>
-      </div>
-      <div className='premium'>
-        <a href='./upgrade'>
-          <button type='button' className='create-b'>
-            Upgrade
-          </button>
-        </a>
-      </div>
-      <div className='languages'>
-        <a href='./language'>
-          <button type='button' className='create-b'>
-            Change Language
-          </button>
-        </a>
-      </div>
+        <Link to='./newpost' className='create'>
+            <button type='button' className='create-b'>Create Post</button>
+        </Link>
+        <Link to='./boards' className='boards-link'>
+            <button type='button' className='create-b'>Boards</button>
+        </Link>
+        <Link to='./premium' className='premium'>
+            <button type='button' className='create-b'>Upgrade</button>
+        </Link>
+        <Link to='./language' className='languages'>
+            <button type='button' className='create-b'>Change Language</button>
+        </Link>
     </div>
   );
 
