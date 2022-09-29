bb28c0f8d3e2c2007ef4df8ce4fa5b46bf5f1f7c
Seth Miller <millerado@gmail.com>
2022-09-29 11:58:42 -0600

    import Link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,36 @@
+import { Link } from 'react-router-dom';
+
 export default function Sidebar(props) {
-    return (
-        <div className="sb-container">
-            <div className="create">
-                <a href="./newpost"><button type="button" className="create-b">Create Post</button></a>
-            </div>
-            <div className="boards">
-                <a href="./boards"><button type="button" className="create-b">Boards</button></a>
-            </div>
-            <div className="premium">
-                <a href="./upgrade"><button type="button" className="create-b">Upgrade</button></a>
-            </div>
-            <div className="languages">
-                <a href="./language"><button type="button" className="create-b">Change Language</button></a>
-            </div>
-        </div>
-    )
+  return (
+    <div className='sb-container'>
+      <div className='create'>
+        <a href='./newpost'>
+          <button type='button' className='create-b'>
+            Create Post
+          </button>
+        </a>
+      </div>
+      <div className='boards'>
+        <a href='./boards'>
+          <button type='button' className='create-b'>
+            Boards
+          </button>
+        </a>
+      </div>
+      <div className='premium'>
+        <a href='./upgrade'>
+          <button type='button' className='create-b'>
+            Upgrade
+          </button>
+        </a>
+      </div>
+      <div className='languages'>
+        <a href='./language'>
+          <button type='button' className='create-b'>
+            Change Language
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 }
-  
